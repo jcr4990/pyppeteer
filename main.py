3010f70941bae2e49fb8cb15cfb804c6f018bca1
jcr4990 <jcr4990@gmail.com>
2020-07-12 02:01:46 -0400

    price avg testing

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import asyncio
 from pyppeteer.launcher import launch
 
 URL = "https://www.gideonsgallery.com/shop-all"
+prices = []
 
 async def main():
-    browser = await launch()
+    browser = await launch() # headless=False
     page = await browser.newPage()
     await page.setViewport({'width': 1920, 'height': 1080})
     await page.goto(URL, {'waitUntil': 'networkidle2'})
@@ -24,8 +25,20 @@ async def main():
     await asyncio.sleep(2)
     await page.screenshot({'path': 'test.png'})
     
+    await asyncio.sleep(2)
+    prices = await page.querySelectorAll("[data-hook='product-item-price-to-pay']")
+    for element in prices:
+        try:
+            element = await element.getProperty('textContent')
+            element = await element.jsonValue()
+            price = element.replace("$","").replace(".00","").replace(",","")
+            prices.append(price)
+        except AttributeError:
+            pass
 
-    
+    for i in prices:
+        print(i)
+
     # PRINT TITLES
     # elements = await page.querySelectorAll("[data-hook='product-item-name']")
 
